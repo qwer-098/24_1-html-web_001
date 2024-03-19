755277399ba03804c27bec5001d4fed503fc3b4c
qwer-098 <163095347+qwer-098@users.noreply.github.com>
2024-03-19 17:56:15 +0900

    002

---
+++ WEB002/night_day.js
@@ -0,0 +1,11 @@
+function nightDay(self) {
+  if (value === "night") {
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("body").style.color = "white";
+    value = "day";
+  } else {
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.color = "black";
+    value = "night";
+  }
+}
